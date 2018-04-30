d1a3a33fe693655865335d9b39f6725ad27e6ea5
Joe Jobes <jmrjobes@gmail.com>
2018-04-30 07:11:14 -0600

    Add comments

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $('#new_comment_button').click(function(){
+    $('#new_comment_button').hide();
+    $('#new_comment').show();
+    $("#new_comment").submit(function(){
+      let comment = $("#comment").val();
+      let authorName = $("#authorName").val();
+      $("#comment_list").append("<li>" + comment + "<span class='author'>" + authorName + "</span></li>")
+      $("#new_comment").hide();
+      $("#new_comment_button").show();
+      event.preventDefault();
+    })
+  });
+});
